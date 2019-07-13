63874012bdbffe41256563446f93bbb862fa430b
Solcan Moise <moise.solcan@paddypowerbetfair.com>
2019-07-13 22:39:50 +0300

    Example of non synchronized program

---
+++ src/threads/Callme.java
@@ -0,0 +1,47 @@
+package threads;
+
+/**
+ * This program is not synchronized
+ */
+public class Callme {
+
+    void call(String msg) {
+
+        System.out.println("[" + msg);
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            System.out.println("Interrupted");
+        }
+        System.out.println("]");
+    }
+
+}
+
+class Caller implements Runnable {
+
+    String msg;
+    Callme target;
+    Thread t;
+
+    public Caller(Callme target, String msg) {
+        this.target = target;
+        this.msg = msg;
+        t = new Thread(this);
+        t.start();
+    }
+
+    @Override
+    public void run() {
+        target.call(msg);
+    }
+}
+
+class Synch {
+
+    public static void main(String[] args) {
+        Callme target = new Callme();
+        Caller c1 = new Caller(target, "Hello");
+        Caller c2 = new Caller(target, "Synchronized");
+    }
+}
